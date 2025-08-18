e12b472f97eedbca4762e779bf05f58ebf2e63fc
Tajim Islam <tajimislam00882@gmail.com>
2025-08-18 09:26:41 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  "projectId": "digital-emporium-l6iku",
+  "appId": "1:852119500810:web:8cd39e8f136b232bda47f3",
+  "storageBucket": "digital-emporium-l6iku.firebasestorage.app",
+  "apiKey": "AIzaSyBS4DRBmHVLVCRFGjKzN4lSoRVvLLmmeV0",
+  "authDomain": "digital-emporium-l6iku.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "852119500810"
 };
 
 // Initialize Firebase
